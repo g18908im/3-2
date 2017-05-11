e4297feb632eafb850857441fff4af7cfff4cc8c
g15908hs <g15908hs@s20641.tsuda.ac.jp>
2017-05-11 18:43:37 +0900

    エラーをなくしました

--- snake-camel/src/snakecamel/SnakeCamelUtil.java
+++ snake-camel/src/snakecamel/SnakeCamelUtil.java
@@ -5,7 +5,7 @@ public class SnakeCamelUtil {
 	public static String snakeToCamelcase(String snake_case) {
 		String[] words = snake_case.split("_");
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < words.length - 1; i++) {
+		for (int i = 0; i < words.length; i++) {
 			sb.append(capitalize(words[i]));
 		}
 		return new String(sb);
@@ -15,17 +15,18 @@ public class SnakeCamelUtil {
 		StringBuilder sb = new StringBuilder();
 		int j = 0;
 		for (int i = 0; i < camelcase.length(); i++) {
-			char c = camelcase.charAt(i);
-			if (Character.isUpperCase(c)) {
-				sb.append(camelcase.substring(j, i));
-				if (sb.length() > 0) {
-					sb.append("_");
+			char c = camelcase.charAt(i); //i番目の文字をcに格納
+			if (Character.isUpperCase(c)) { //cが大文字ならば
+				sb.append(camelcase.substring(j, i)); //j番目からi-1番目の文字を追加
+				//sb.deleteCharAt(j+1);
+				if (sb.length() > 0) { //sbの長さが0より大きければ
+					sb.append("_");//_を追加
 				}
-				sb.append(Character.toLowerCase(c));
-				j = i;
+				sb.append(Character.toLowerCase(c));//cをすべて小文字にする
+				j = i+1; 
 			}
 		}
-		sb.append(camelcase.substring(j));
+		sb.append(camelcase.substring(j));//文字の最後尾を追加
 		return new String(sb);
 	}
 	
@@ -37,10 +38,10 @@ public class SnakeCamelUtil {
 	}
 
 	static String uncapitalize(String s) {
-		char first = s.charAt(0);
-		char lowerFirst = Character.toLowerCase(first);
-		String rest = s.substring(1);
-		return lowerFirst + rest;
+		char first = s.charAt(0);//sの0番目の文字をfirstに格納
+		char lowerFirst = Character.toLowerCase(first);//firstの小文字をlowerFirstを格納
+		String rest = s.substring(1);//sの1番目から末尾まで文字をrestに格納
+		return lowerFirst + rest;//firstの小文字+1番目から末尾→すべてを小文字にした
 	}
 	
 }
